fe6f495774709c839700f7ac6a95a89f28a7bce1
sdlm <sdlmerr@yandex.ru>
2019-09-01 16:12:23 +0300

    temp #0

--- src/handlers.py
+++ src/handlers.py
@@ -3,6 +3,16 @@ from flask import Blueprint
 webapp = Blueprint("webapp", __name__)
 
 
+@webapp.route("/get_the_same_img")
+def get_the_same_img():
+    return {"get_the_same_img": "ok"}
+
+
+@webapp.route("/get_square_img")
+def get_square_img():
+    return {"get_square_img": "ok"}
+
+
 @webapp.route("/card_exists")
 def card_exists():
     return {"card_exists": "ok"}
---
+++ src/predict_existence.py
@@ -0,0 +1,30 @@
+import torch
+from PIL import Image
+from torch import nn
+from torchvision import models
+from torchvision.transforms import transforms
+
+MODEL_PATH = "./data/weights/resnet18_classification_v1.1.2.pt"
+
+
+def load_model(weights_path: str = None):
+    model = models.resnet50(pretrained=True)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 2)
+
+    path = weights_path if weights_path else MODEL_PATH
+    model.load_state_dict(torch.load(path))
+    model.eval()
+
+    return model
+
+
+def predict_existence(model, tensor) -> bool:
+    predicted_labels = model(tensor)
+    return predicted_labels[0][0].item() < predicted_labels[0][1].item()
+
+
+def get_card_existence(img: Image) -> bool:
+    model = load_model()
+    tensor = transforms.ToTensor()(img).unsqueeze(0)
+    return predict_existence(model, tensor)
